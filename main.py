b4d6b0c8b8af36320ad832410a7c5f91c69e10c4
diogenisAl <diogenis_al@yahoo.com>
2022-03-25 23:24:56 +0200

    small changes

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import json
 app = flask.Flask(__name__)
 # app.config["DEBUG"] = True
 
-#  Open data.json file
+#  Open .json file
 f = open('data/members_data.json', "r")
 
 #  Load data from json file to data variable.
-animals = json.load(f)
+members = json.load(f)
 
 
 @app.route('/', methods=['GET'])
@@ -21,33 +21,25 @@ def home():
 # A route to return all of the available entries in our catalog.
 @app.route('/api/members/all', methods=['GET'])
 def api_all():
-    return jsonify(animals)
+    return jsonify(members)
 
 @app.route('/api/members', methods=['GET'])
 def api_id():
-    # Check if an English Name was provided as part of the URL.
-    # If English Name is provided, assign it to a variable.
-    # If no English Name is provided, display an error in the browser.
-    # URL example: http://127.0.0.1:5000/api/animals?english_name=Dog
 
-    #if 'id' in request.args:
-    #    id = int(request.args['id'])
-
-    if 'english_name' in request.args:
-        english_name = str(request.args['english_name'])
+    if 'id' in request.args:
+        id = str(request.args['id'])
     else:
 
-        return "Error: No english_name field provided. Please specify an english_name."
+        return "Error: No id field provided. Please specify an id."
 
     # Create an empty list for our results
     results = []
 
     # Loop through the data and match results that fit the requested ID.
     # IDs are unique, but other fields might return many results
-    for animal in animals:
-#        if animal['id'] == id:
-        if animal['english_name'] == english_name:
-            results.append(animal)
+    for member in members:
+        if member['members_IDs'].find(id) != -1:
+            results.append(member)
 
     # Use the jsonify function from Flask to convert our list of
     # Python dictionaries to the JSON format.
